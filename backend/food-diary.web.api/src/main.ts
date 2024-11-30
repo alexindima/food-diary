6aa245bd8e3393e20971d500b1d4b1f5d76ec15f
AlexinD <dalexin@codemastersintl.com>
2024-11-30 05:33:22 +0400

    try GitHub actions #27

--- backend/food-diary.web.api/src/main.ts
+++ backend/food-diary.web.api/src/main.ts
@@ -37,7 +37,7 @@ async function bootstrap() {
         .build();
 
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('swagger', app, document);
+    SwaggerModule.setup('api/swagger', app, document);
 
     app.setGlobalPrefix('api');
     const port = process.env.PORT || 3000;
