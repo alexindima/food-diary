9a7d03997ba40a9fbb4833c6646fda5ead880d2b
Dmitrii Aleksin <alexindima@gmail.com>
2026-02-10 01:07:49 +0400

    Refactor recipe card: optimize grid layout, enhance responsiveness, consolidate nutrient badges, and remove unused functions.

--- FoodDiary.Web.Client/src/app/components/shared/recipe-card/recipe-card.component.ts
+++ FoodDiary.Web.Client/src/app/components/shared/recipe-card/recipe-card.component.ts
@@ -4,22 +4,17 @@ import { TranslatePipe } from '@ngx-translate/core';
 import { FdUiIconModule } from 'fd-ui-kit/material';
 import { FdUiButtonComponent } from 'fd-ui-kit/button/fd-ui-button.component';
 import { Recipe } from '../../../types/recipe.data';
-import { resolveProductImageUrl } from '../../../utils/product-stub.utils';
-import { resolveRecipeImageUrl } from '../../../utils/recipe-stub.utils';
-import { ProductType } from '../../../types/product.data';
+import { NutrientBadgesComponent } from '../nutrient-badges/nutrient-badges.component';
 
 @Component({
     selector: 'fd-recipe-card',
     standalone: true,
-    imports: [CommonModule, TranslatePipe, FdUiIconModule, FdUiButtonComponent],
+    imports: [CommonModule, TranslatePipe, FdUiIconModule, FdUiButtonComponent, NutrientBadgesComponent],
     templateUrl: './recipe-card.component.html',
     styleUrl: './recipe-card.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RecipeCardComponent {
-    private readonly fallbackIngredientImage = 'assets/images/stubs/receipt.png';
-    private readonly previewLimit = 3;
-
     @Input({ required: true }) recipe!: Recipe;
     @Input() imageUrl?: string;
     @Output() open = new EventEmitter<void>();
@@ -47,48 +42,5 @@ export class RecipeCardComponent {
         }
 
         return this.recipe.steps.reduce((total, step) => total + (step.ingredients?.length ?? 0), 0);
-    }
-
-    public previewImages(): string[] {
-        const images = this.ingredientImages();
-
-        if (images.length === 0) {
-            return [this.fallbackIngredientImage];
-        }
-
-        return images.slice(0, this.previewLimit);
-    }
-
-    public extraCount(): number {
-        const total = this.ingredientImages().length;
-        return Math.max(0, total - this.previewLimit);
-    }
-
-    private ingredientImages(): string[] {
-        if (!this.recipe?.steps?.length) {
-            return [];
-        }
-
-        const images: string[] = [];
-
-        for (const step of this.recipe.steps) {
-            for (const ingredient of step.ingredients ?? []) {
-                if (ingredient.productId) {
-                    const image = resolveProductImageUrl(undefined, ProductType.Unknown) ?? this.fallbackIngredientImage;
-                    images.push(image);
-                    continue;
-                }
-
-                if (ingredient.nestedRecipeId) {
-                    const image = resolveRecipeImageUrl(undefined) ?? this.fallbackIngredientImage;
-                    images.push(image);
-                    continue;
-                }
-
-                images.push(this.fallbackIngredientImage);
-            }
-        }
-
-        return images;
     }
 }
