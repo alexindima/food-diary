89827f9afa2fb6b050e5fbf9e681fcc09c9d2fa8
Dmitrii Aleksin <alexindima@gmail.com>
2026-04-11 02:43:06 +0400

    feat(migrations): add migration for push notification preferences

--- FoodDiary.Infrastructure/Migrations/20260410133259_AddPushNotificationsPreferences.cs
+++ FoodDiary.Infrastructure/Migrations/20260410133259_AddPushNotificationsPreferences.cs
@@ -1,51 +1,47 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace FoodDiary.Infrastructure.Migrations
-{
+namespace FoodDiary.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class AddPushNotificationsPreferences : Migration {
     /// <inheritdoc />
-    public partial class AddPushNotificationsPreferences : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<bool>(
-                name: "FastingPushNotificationsEnabled",
-                table: "Users",
-                type: "boolean",
-                nullable: false,
-                defaultValue: true);
+    protected override void Up(MigrationBuilder migrationBuilder) {
+        migrationBuilder.AddColumn<bool>(
+            name: "FastingPushNotificationsEnabled",
+            table: "Users",
+            type: "boolean",
+            nullable: false,
+            defaultValue: true);
 
-            migrationBuilder.AddColumn<bool>(
-                name: "PushNotificationsEnabled",
-                table: "Users",
-                type: "boolean",
-                nullable: false,
-                defaultValue: false);
+        migrationBuilder.AddColumn<bool>(
+            name: "PushNotificationsEnabled",
+            table: "Users",
+            type: "boolean",
+            nullable: false,
+            defaultValue: false);
 
-            migrationBuilder.AddColumn<bool>(
-                name: "SocialPushNotificationsEnabled",
-                table: "Users",
-                type: "boolean",
-                nullable: false,
-                defaultValue: true);
-        }
+        migrationBuilder.AddColumn<bool>(
+            name: "SocialPushNotificationsEnabled",
+            table: "Users",
+            type: "boolean",
+            nullable: false,
+            defaultValue: true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "FastingPushNotificationsEnabled",
-                table: "Users");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) {
+        migrationBuilder.DropColumn(
+            name: "FastingPushNotificationsEnabled",
+            table: "Users");
 
-            migrationBuilder.DropColumn(
-                name: "PushNotificationsEnabled",
-                table: "Users");
+        migrationBuilder.DropColumn(
+            name: "PushNotificationsEnabled",
+            table: "Users");
 
-            migrationBuilder.DropColumn(
-                name: "SocialPushNotificationsEnabled",
-                table: "Users");
-        }
+        migrationBuilder.DropColumn(
+            name: "SocialPushNotificationsEnabled",
+            table: "Users");
     }
 }
