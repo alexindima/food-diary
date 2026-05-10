ddfd3909a644aaf82bc4206157e096d93dc98b90
Dmitrii Aleksin <alexindima@gmail.com>
2026-05-10 12:34:28 +0400

    refactor basic info form errors

--- FoodDiary.Web.Client/src/app/features/products/components/manage/product-basic-info/product-basic-info.component.ts
+++ FoodDiary.Web.Client/src/app/features/products/components/manage/product-basic-info/product-basic-info.component.ts
@@ -1,11 +1,12 @@
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, input, output, signal } from '@angular/core';
 import { type FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { FdUiAutocompleteComponent, type FdUiAutocompleteOption } from 'fd-ui-kit/autocomplete/fd-ui-autocomplete.component';
 import { FdUiCardComponent } from 'fd-ui-kit/card/fd-ui-card.component';
 import { FdUiInputComponent } from 'fd-ui-kit/input/fd-ui-input.component';
 import { FdUiSelectComponent, type FdUiSelectOption } from 'fd-ui-kit/select/fd-ui-select.component';
 import { FdUiTextareaComponent } from 'fd-ui-kit/textarea/fd-ui-textarea.component';
+import { EMPTY, merge, type Observable } from 'rxjs';
 
 import { ImageUploadFieldComponent } from '../../../../../components/shared/image-upload-field/image-upload-field.component';
 import { type MeasurementUnit, type ProductSearchSuggestion, type ProductType, type ProductVisibility } from '../../../models/product.data';
@@ -14,6 +15,10 @@ import { type ProductFormData } from '../base-product-manage.component';
 export type ProductNameSuggestion = ProductSearchSuggestion;
 
 export type ProductNameAutocompleteOption = FdUiAutocompleteOption<string> & { data: ProductNameSuggestion };
+
+const ERROR_FIELDS = ['name', 'productType', 'defaultPortionAmount', 'baseUnit', 'visibility'] as const;
+type ErrorField = (typeof ERROR_FIELDS)[number];
+type FieldErrors = Record<ErrorField, string | null>;
 
 @Component({
     selector: 'fd-product-basic-info',
@@ -33,6 +38,8 @@ export type ProductNameAutocompleteOption = FdUiAutocompleteOption<string> & { d
     ],
 })
 export class ProductBasicInfoComponent {
+    private readonly translateService = inject(TranslateService);
+
     public readonly formGroup = input.required<FormGroup<ProductFormData>>();
     public readonly unitOptions = input.required<FdUiSelectOption<MeasurementUnit>[]>();
     public readonly productTypeOptions = input.required<FdUiSelectOption<ProductType>[]>();
@@ -40,6 +47,7 @@ export class ProductBasicInfoComponent {
     public readonly nameOptions = input.required<ProductNameAutocompleteOption[]>();
     public readonly isNameSearchLoading = input.required<boolean>();
     public readonly getControlError = input.required<(controlName: keyof ProductFormData) => string | null>();
+    public readonly fieldErrors = signal<FieldErrors>(this.createEmptyFieldErrors());
 
     public openBarcodeScanner = output<void>();
     public openAiRecognition = output<void>();
@@ -48,10 +56,45 @@ export class ProductBasicInfoComponent {
 
     public readonly displayNameValue = (value: string | null): string => value ?? '';
 
+    private readonly errorSync = effect(onCleanup => {
+        const form = this.formGroup();
+        const formEvents = (form as { events?: Observable<unknown> }).events ?? EMPTY;
+        const languageChanges = (this.translateService as { onLangChange?: Observable<unknown> }).onLangChange ?? EMPTY;
+        const refresh = (): void => {
+            this.fieldErrors.set(this.buildFieldErrors());
+        };
+
+        refresh();
+        const subscription = merge(formEvents, form.statusChanges, form.valueChanges, languageChanges).subscribe(() => {
+            refresh();
+        });
+        onCleanup(() => {
+            subscription.unsubscribe();
+        });
+    });
+
     public onNameOptionSelected(option: FdUiAutocompleteOption<string>): void {
         const suggestion = option.data as ProductNameSuggestion | undefined;
         if (suggestion) {
             this.nameSuggestionSelected.emit(suggestion);
         }
+    }
+
+    private buildFieldErrors(): FieldErrors {
+        const getControlError = this.getControlError();
+        return ERROR_FIELDS.reduce<FieldErrors>((errors, field) => {
+            errors[field] = getControlError(field);
+            return errors;
+        }, this.createEmptyFieldErrors());
+    }
+
+    private createEmptyFieldErrors(): FieldErrors {
+        return {
+            name: null,
+            productType: null,
+            defaultPortionAmount: null,
+            baseUnit: null,
+            visibility: null,
+        };
     }
 }
--- FoodDiary.Web.Client/src/app/features/recipes/components/manage/recipe-basic-info/recipe-basic-info.component.ts
+++ FoodDiary.Web.Client/src/app/features/recipes/components/manage/recipe-basic-info/recipe-basic-info.component.ts
@@ -1,14 +1,19 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, input, signal } from '@angular/core';
 import { type AbstractControl, type FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { FdUiCardComponent } from 'fd-ui-kit/card/fd-ui-card.component';
 import { FdUiInputComponent } from 'fd-ui-kit/input/fd-ui-input.component';
 import { FdUiSelectComponent, type FdUiSelectOption } from 'fd-ui-kit/select/fd-ui-select.component';
 import { FdUiTextareaComponent } from 'fd-ui-kit/textarea/fd-ui-textarea.component';
+import { EMPTY, merge, type Observable } from 'rxjs';
 
 import { ImageUploadFieldComponent } from '../../../../../components/shared/image-upload-field/image-upload-field.component';
 import { type RecipeVisibility } from '../../../models/recipe.data';
 import { type RecipeFormData } from '../recipe-manage.types';
+
+const ERROR_FIELDS = ['name', 'cookTime', 'prepTime', 'servings', 'description', 'visibility', 'comment'] as const;
+type ErrorField = (typeof ERROR_FIELDS)[number];
+type FieldErrors = Record<ErrorField, string | null>;
 
 @Component({
     selector: 'fd-recipe-basic-info',
@@ -31,9 +36,42 @@ export class RecipeBasicInfoComponent {
 
     public readonly formGroup = input.required<FormGroup<RecipeFormData>>();
     public readonly visibilitySelectOptions = input.required<FdUiSelectOption<RecipeVisibility>[]>();
+    public readonly fieldErrors = signal<FieldErrors>(this.createEmptyFieldErrors());
 
-    public getFieldError(controlName: keyof RecipeFormData): string | null {
-        return this.resolveControlError(this.formGroup().controls[controlName]);
+    private readonly errorSync = effect(onCleanup => {
+        const form = this.formGroup();
+        const formEvents = (form as { events?: Observable<unknown> }).events ?? EMPTY;
+        const languageChanges = (this.translateService as { onLangChange?: Observable<unknown> }).onLangChange ?? EMPTY;
+        const refresh = (): void => {
+            this.fieldErrors.set(this.buildFieldErrors(form));
+        };
+
+        refresh();
+        const subscription = merge(formEvents, form.statusChanges, form.valueChanges, languageChanges).subscribe(() => {
+            refresh();
+        });
+        onCleanup(() => {
+            subscription.unsubscribe();
+        });
+    });
+
+    private buildFieldErrors(form: FormGroup<RecipeFormData>): FieldErrors {
+        return ERROR_FIELDS.reduce<FieldErrors>((errors, field) => {
+            errors[field] = this.resolveControlError(form.controls[field]);
+            return errors;
+        }, this.createEmptyFieldErrors());
+    }
+
+    private createEmptyFieldErrors(): FieldErrors {
+        return {
+            name: null,
+            cookTime: null,
+            prepTime: null,
+            servings: null,
+            description: null,
+            visibility: null,
+            comment: null,
+        };
     }
 
     private resolveControlError(control: AbstractControl | null): string | null {
